85e6593815c71324557079175578725259e732bb
dmitriytimoshenko <asassinich@gmail.com>
2020-05-15 10:36:41 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,31 @@
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.main.Main;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+        /*CamelContext context = new DefaultCamelContext();
+        context.start();
+        try {
+            Thread.sleep(4000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        context.stop();
+        */
+        Main main = new Main();
+        main.configure();
+
+        try {
+            main.run(args);
+            System.out.println(main.getConfigurationClasses());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
